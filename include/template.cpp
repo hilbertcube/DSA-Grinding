319801571480dc466ec9eda0fd4b9b355f5db5cd
Don Le <ledongduu@gmail.com>
2026-06-16 02:06:08 -0700

    updated default template

--- include/template.cpp
+++ include/template.cpp
@@ -6,6 +6,11 @@ void implementation(vector<int> &const input) {
 #ifdef TESTING
 #include <gtest/gtest.h>
 
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
 #else
 
 int main() {
